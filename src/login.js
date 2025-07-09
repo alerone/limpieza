397cb16847f7604c3cd3186e277ae19afb8775ad
Alvaro <lopezalvarezalvaro1@gmail.com>
2025-07-09 13:22:25 +0200

    design: improved the front login design

--- src/login.js
+++ src/login.js
@@ -1,13 +1,12 @@
-import { handleLogin } from './auth.js'
+import { auth, handleLogin } from './auth.js'
+import { onAuthStateChanged } from 'firebase/auth'
 
-const loginButton = document.querySelector('#loginButton')
+const loginButton = document.querySelector('#loginBtn')
 
-if (loginButton) {
-    loginButton.addEventListener("click", handleLogin)
-    onAuthStateChanged(auth, (user) => {
-        if (user) {
-            window.location.href = "dashboard.html"
-        }
-    })
-}
+loginButton.addEventListener("click", handleLogin)
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        window.location.href = "dashboard.html"
+    }
+})
 
