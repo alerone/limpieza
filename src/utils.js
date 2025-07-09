a6ca36a31f8e4b45f89d50de4e36690c2ba141a9
Alvaro <lopezalvarezalvaro1@gmail.com>
2025-07-09 14:10:07 +0200

    design: dashboard week format changed

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-export function getDayString() {
+export function getPreciseDateString() {
     const hoy = new Date()
     const dia = String(hoy.getDate()).padStart(2, '0')
     const mes = String(hoy.getMonth() + 1).padStart(2, '0') // Los meses van de 0 a 11
@@ -7,4 +7,25 @@ export function getDayString() {
     const minutos = hoy.getMinutes()
 
     return `${dia}/${mes}/${año} ${hora}:${minutos}`
+}
+
+export function getDayString(date) {
+    const dia = String(date.getDate()).padStart(2, '0')
+    const mes = String(date.getMonth() + 1).padStart(2, '0') // Los meses van de 0 a 11
+    const año = date.getFullYear()
+
+    return `${dia}/${mes}/${año}`
+}
+
+export function getWeekBounds(date) {
+    const current = new Date(date);
+    const day = current.getDay();
+    const diffToMonday = day === 0 ? -6 : 1 - day;
+
+    const monday = new Date(current);
+    monday.setDate(current.getDate() + diffToMonday);
+
+    const sunday = new Date(monday);
+    sunday.setDate(monday.getDate() + 6);
+    return `${getDayString(monday)} - ${getDayString(sunday)}`
 }
