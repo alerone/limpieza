db0d86987db0a29d38d7e454cb2ffe28c70c3c6b
alerone <alerone@users.noreply.github.com>
2025-07-10 10:44:53 +0000

    deploy: cf9627759e888c74f1b96cc327d9916da42c4f91

--- assets/login-H7kRFp70.js
+++
@@ -1,1 +0,0 @@
-import{h as t,o as n,a}from"./auth-CzND0SIy.js";const e=document.querySelector("#loginBtn");e.addEventListener("click",t);n(a,o=>{o&&(window.location.href="./dashboard.html")});
---
+++ assets/login-M9d6z6v6.js
@@ -0,0 +1,1 @@
+import{h as t,o as n,a}from"./auth-d8-T1P3Y.js";const e=document.querySelector("#loginBtn");e.addEventListener("click",t);n(a,o=>{o&&(window.location.href="./dashboard.html")});
