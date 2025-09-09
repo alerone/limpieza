bf833eaa5964a88a1a85854634c46c798b4fe3e1
Álvaro <lopezalvarezalvaro1@gmail.com>
2025-09-09 10:21:18 +0200

    isIosSafari commented

--- src/utils/isIOS.ts
+++ src/utils/isIOS.ts
@@ -1,4 +1,4 @@
-export function isIOSSafari() {
-    const ua = window.navigator.userAgent
-    return /IP(ad|hone|od)/.test(ua) && /Safari/.test(ua) && !/CriOS|FxiOS/.test(ua)
-}
+// export function isIOSSafari() {
+//     const ua = window.navigator.userAgent
+//     return /IP(ad|hone|od)/.test(ua) && /Safari/.test(ua) && !/CriOS|FxiOS/.test(ua)
+// }
